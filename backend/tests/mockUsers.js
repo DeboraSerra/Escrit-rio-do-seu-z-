97820996bb30d9d3b65f3de4d03ec0eaa2bbd41d
Débora Serra <debora.r.serra@gmail.com>
2022-06-13 16:25:36 -0300

    feat: add tests for the functions readUser and addUser

---
+++ backend/tests/mockUsers.js
@@ -0,0 +1,16 @@
+const mockUsers = [
+  {
+    "email":"debora.r.serra@gmail.com",
+    "password":"debs123!",
+    "name":"Débora Serra",
+    "id":1
+  },
+  {
+    "email":"tryber@tryber.com",
+    "password":"trybe12!",
+    "name":"Tryber",
+    "id":2
+  }
+]
+
+module.exports = mockUsers;
